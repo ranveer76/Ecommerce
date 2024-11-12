cb4143c8f38fdabd46a1417ddc3d2d365278ae32
Ranveer Singh Walia <ranveerwalia76@gmail.com>
2024-11-12 14:45:27 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get(express.static('public'));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 
 app.use('/api', (req, res) => {
@@ -20,9 +20,9 @@ app.use('/api', (req, res) => {
 });
 
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.use((req, res) => {
     res.status(404).json({ message: '404 Not Found' });
