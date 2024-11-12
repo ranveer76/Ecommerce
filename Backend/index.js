cf0cc58daefa95125691c1ece43aaa825b0a4a28
Ranveer Singh Walia <ranveerwalia76@gmail.com>
2024-11-12 14:39:51 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get(express.static(path.join(__dirname, 'public')));
+app.get(express.static('public'));
 
 
 app.use('/api', (req, res) => {
